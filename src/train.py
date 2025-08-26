0b8cc146dcef4073b03fb69d5c76fa357c2e2295
NotXia <35894453+NotXia@users.noreply.github.com>
2025-08-26 16:57:54 +0900

    Add checkpoint loading and mixed precision

--- src/train.py
+++ src/train.py
@@ -20,10 +20,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Model training")
     parser.add_argument("--model-card", type=str, required=True, help="Text encoder backbone")
     parser.add_argument("--dataset", type=str, required=True, choices=DATASETS, help="Training dataset")
+    parser.add_argument("--checkpoint", type=str, required=False, help="Checkpoint to load")
     parser.add_argument("--phase", type=str, required=True, choices=TRAINING_PHASES, help="Training phase")
     parser.add_argument("--epochs", type=int, required=True)
     parser.add_argument("--gpus", type=int, default=1, help="Number of GPUs")
     parser.add_argument("--batch-size", type=int, required=True)
+    parser.add_argument("--mixed-precision", action="store_true", default=False, help="Use b16 mixed precision")
     parser.add_argument("--data-dir", type=str, default="./_datasets", help="Directory where the dataset is located/downloaded")
     parser.add_argument("--model-dir", type=str, default="./_model", help="Directory where the weights and logs are saved into")
     parser.add_argument("--seed", type=int, default=42, help="Initialization seed")
@@ -31,10 +33,6 @@ if __name__ == "__main__":
     
     L.seed_everything(args.seed)
 
-    sep_tok, sep_tok_id = get_sep_token(args.model_card)
-    ds_train, ds_val, num_classes = get_dataset(args.dataset, args.data_dir, sep_tok, ["train", "validation"])
-
-    model = TextClaimVerificationModel(args.model_card, out_classes=num_classes, training_phase=args.phase)
 
     trainer = L.Trainer(
         max_epochs = args.epochs,
@@ -44,8 +42,26 @@ if __name__ == "__main__":
             ModelCheckpoint(dirpath=os.path.join(args.model_dir, "checkpoints"), save_top_k=1, save_last=True, monitor="val_loss", mode="min"),
             EarlyStopping(monitor="val_loss", patience=3, mode="min"),
         ],
-        devices = args.gpus
+        strategy = "ddp_find_unused_parameters_true",
+        devices = args.gpus,
+        precision = "bf16-mixed" if args.mixed_precision else None
     )
+
+
+    sep_tok, sep_tok_id = get_sep_token(args.model_card)
+    ds_train, ds_val, num_classes = get_dataset(args.dataset, args.data_dir, sep_tok, ["train", "validation"])
+
+    if args.checkpoint is not None:
+        with trainer.init_module(empty_init=True):
+            model = TextClaimVerificationModel.load_from_checkpoint(
+                args.checkpoint,
+                map_location = "cpu",
+                text_encoder_card = args.model_card, 
+                out_classes = num_classes, 
+                training_phase = args.phase
+            )
+    else:
+        model = TextClaimVerificationModel(args.model_card, out_classes=num_classes, training_phase=args.phase)
 
     trainer.fit(
         model,
