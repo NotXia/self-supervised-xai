bbe59cec9d4d45409a3ae950ef8f8ac32b298285
NotXia <35894453+NotXia@users.noreply.github.com>
2026-01-30 16:45:05 +0900

    Add training script

--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import json
+from pathlib import Path
 
 import torch
 from torch.utils.data import DataLoader
@@ -9,8 +10,8 @@ from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.loggers import CSVLogger
 
-from models import get_sep_token, TRAINING_PHASES, TextClaimVerificationModel
-from data import DATASETS, get_dataset
+from models import TRAINING_PHASES, get_model
+from data import get_dataset
 
 
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
@@ -19,68 +20,89 @@ torch.set_float32_matmul_precision("high")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Model training")
-    parser.add_argument("--model-card", type=str, required=True, help="Text encoder backbone")
-    parser.add_argument("--dataset", type=str, required=True, choices=DATASETS, help="Training dataset")
+    parser.add_argument("--model-config", type=str, required=True, help="Path to model config JSON")
     parser.add_argument("--checkpoint", type=str, required=False, help="Checkpoint to load")
-    parser.add_argument("--phase", type=str, required=True, choices=TRAINING_PHASES, help="Training phase")
-    parser.add_argument("--pool-embeds", action="store_true", default=False, help="Use average pooling instead of separator token for sentence embedding")
-    parser.add_argument("--pe-cls", action="store_true", default=False, help="Use positional encoding for masker and classifier")
-    parser.add_argument("--epochs", type=int, required=True)
     parser.add_argument("--gpus", type=str, default="0", help="GPUs indices")
+    parser.add_argument("--epochs", type=int, required=True)
     parser.add_argument("--batch-size", type=int, required=True)
+    parser.add_argument("--grad-accum", type=int, default=1, help="Gradient accumulation steps")
     parser.add_argument("--mixed-precision", action="store_true", default=False, help="Use b16 mixed precision")
-    parser.add_argument("--data-dir", type=str, default="./_datasets", help="Directory where the dataset is located/downloaded")
-    parser.add_argument("--model-dir", type=str, default="./_model", help="Directory where the weights and logs are saved into")
+    parser.add_argument("--phase", type=str, required=True, choices=TRAINING_PHASES, help="Training phase")
+    parser.add_argument("--data-dir", type=str, default="../_datasets", help="Directory where the dataset is located/downloaded")
+    parser.add_argument("--model-dir", type=str, required=False, 
+        help="Directory where the weights and logs are saved into. If not set, the same directory of the model config will be used")
     parser.add_argument("--seed", type=int, default=42, help="Initialization seed")
     args = parser.parse_args()
     
     L.seed_everything(args.seed)
-    os.makedirs(args.model_dir, exist_ok=True)
 
+    # Get model configuration
+    model_config = json.load( open(args.model_config, "r") )
+    config_dir = Path(args.model_config).parent
+
+    # Prepare output directory
+    model_dir = os.path.join(config_dir, args.phase) if args.model_dir is None else args.model_dir
+    os.makedirs(model_dir, exist_ok=True)
+
+    # Prepare trainer
     trainer = L.Trainer(
         max_epochs = args.epochs,
-        logger = CSVLogger(os.path.join(args.model_dir, "logs")),
+        logger = CSVLogger(os.path.join(model_dir, "logs")),
         log_every_n_steps = 1,
         callbacks = [
-            ModelCheckpoint(dirpath=os.path.join(args.model_dir, "checkpoints"), save_top_k=1, save_last=True, monitor="val_loss", mode="min"),
-            EarlyStopping(monitor="val_loss", patience=3, mode="min"),
+            # ModelCheckpoint(dirpath=os.path.join(model_dir, "checkpoints"), save_top_k=1, save_last=True, monitor="val_loss", mode="min"),
+            # EarlyStopping(monitor="val_loss", patience=5, mode="min"),
+            ModelCheckpoint(dirpath=os.path.join(model_dir, "checkpoints"), save_top_k=1, save_last=True, monitor="val_acc", mode="max"),
+            EarlyStopping(monitor="val_acc", patience=10, mode="max"),
+
         ],
         strategy = "ddp_find_unused_parameters_true",
         devices = args.gpus,
+        accumulate_grad_batches = args.grad_accum,
         precision = "bf16-mixed" if args.mixed_precision else None
     )
 
 
-    sep_tok, sep_tok_id = get_sep_token(args.model_card)
-    ds_train, ds_val, num_classes = get_dataset(args.dataset, args.data_dir, sep_tok, ["train", "validation"])
+    # Load datasets
+    ds_train, ds_val, num_classes = get_dataset(
+        dataset_name = model_config["dataset_name"],
+        data_dir = args.data_dir, 
+        splits = ["train", "validation"],
+        seed = args.seed,
+        **model_config["dataset_args"]
+    )
 
-    if args.checkpoint is not None:
-        with trainer.init_module(empty_init=True):
-            model = TextClaimVerificationModel.load_from_checkpoint(
-                args.checkpoint,
-                map_location = "cpu",
-                text_encoder_card = args.model_card, 
-                out_classes = num_classes, 
-                training_phase = args.phase,
-                use_pooling = args.pool_embeds,
-                cls_positional_encoding = args.pe_cls
-            )
-    else:
-        model = TextClaimVerificationModel(
-            args.model_card, 
-            out_classes = num_classes, 
-            training_phase = args.phase,
-            use_pooling = args.pool_embeds,
-            cls_positional_encoding = args.pe_cls
-        )
 
-    with open(os.path.join(args.model_dir, "_metadata.json"), "w") as f:
+    # Load model
+    model = get_model(
+        model_name = model_config["model_name"], 
+        checkpoint_path = args.checkpoint, 
+        training_phase = args.phase,
+        **model_config["model_args"]
+    )
+
+
+    # Dump arguments
+    with open(os.path.join(model_dir, "_metadata.json"), "w") as f:
         json.dump({
-            "arguments": args.__dict__
+            "arguments": args.__dict__,
+            "config": model_config
         }, f, indent=4)
 
+
+    # Training
     trainer.fit(
         model,
         DataLoader(ds_train, batch_size=args.batch_size, shuffle=True, num_workers=2), 
         DataLoader(ds_val, batch_size=args.batch_size, shuffle=False, num_workers=2)
     )
+
+
+    # Dump updated metadata
+    with open(os.path.join(model_dir, "_metadata.json"), "w") as f:
+        json.dump({
+            "arguments": args.__dict__,
+            "best_checkpoint": os.path.relpath(trainer.checkpoint_callback.best_model_path, start=os.path.abspath(config_dir)),
+            "best_checkpoint_abs": trainer.checkpoint_callback.best_model_path,
+            "config": model_config
+        }, f, indent=4)
