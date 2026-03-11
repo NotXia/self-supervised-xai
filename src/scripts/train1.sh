7218e44dedd764fe9b25960f68ab7ef0ac8d3d58
NotXia <35894453+NotXia@users.noreply.github.com>
2026-03-11 21:51:40 +0100

    Update training scripts

--- src/scripts/train1.sh
+++ src/scripts/train1.sh
@@ -1,11 +1,9 @@
 cd ..
 python ./train.py \
     --model-config "../configs/$1/config.json" \
-    --checkpoint "$3" \
-    --phase "masker" \
+    --phase "finetune" \
     --gpus "$2," \
-    --epochs 5 \
+    --epochs 10 \
     --batch-size 32 \
-    --mixed-precision \
     --data-dir ../_datasets \
     --seed 42
--- src/scripts/train2.sh
+++ src/scripts/train2.sh
@@ -1,10 +1,10 @@
 cd ..
 python ./train.py \
     --model-config "../configs/$1/config.json" \
-    --phase "finetune" \
+    --checkpoint "$3" \
+    --phase "masker" \
     --gpus "$2," \
-    --epochs 10 \
+    --epochs 5 \
     --batch-size 32 \
-    --mixed-precision \
     --data-dir ../_datasets \
     --seed 42
